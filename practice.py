ce9fa81da579c69fcfbdb457758a831ae81b93d7
daiki-nishimura <daikiahe0@gmail.com>
2020-11-18 22:55:14 +0900

    練習問題

---
+++ practice.py
@@ -0,0 +1,8 @@
+# Camusの文字列を１つずつ出力するには
+
+author = "Camus"
+print(author[0]) # 出力なのでprintが使われる
+print(author[1]) # (この場合文字列のインデックス)[ ] は取り出す時に使われる。
+print(author[2])
+print(author[3])
+print(author[4])
