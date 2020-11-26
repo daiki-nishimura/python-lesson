a2a7a0f3c846d9e250e4bbad4b2a242f064d5603
daiki-nishimura <daikiahe0@gmail.com>
2020-11-26 23:07:54 +0900

    練習問題

---
+++ practice3.py
@@ -0,0 +1,5 @@
+# 大文字にして出力する
+
+# upperは大文字にするメソッド　
+i = "aldous Huxley was born in 1894".upper()
+print(i)
---
+++ practice4.py
@@ -0,0 +1,6 @@
+# lst = "Where now? Who now? When now?".split("?")
+
+# .split()はどこで分割するのか決めるメソッド
+
+lst = "Where now? Who now? When now?".split("?")
+print(lst)
