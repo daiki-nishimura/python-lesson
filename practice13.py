d254982886c8824f568a4a10f8caa85e99f4e861
daiki-nishimura <daikiahe0@gmail.com>
2020-12-03 16:10:03 +0900

    練習問題１３

---
+++ practice13.py
@@ -0,0 +1,13 @@
+# 三角形を表すTriangleクラスを定義して面積を返すareaメソッドを持たせる。そしてTriangleオブジェクトを作ってareaメソッドを呼び出して結果を出力する。
+
+class Triangle():
+
+    def __init__(self,b, h):
+        self.bottom = b
+        self.height = h
+
+    def triangle_area(self):
+        return self.bottom * self.height / 2
+
+a_triangle = Triangle(10, 2)
+print(a_triangle.triangle_area())
