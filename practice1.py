7a38b3e820caada24386d7ff4643fafcdc1947e4
daiki-nishimura <72265603+daiki-nishimura@users.noreply.github.com>
2020-12-13 10:46:59 +0900

    Feature/practice1 (#12)
    * 練習問題１４
    
    * 練習問題２
    
    * 練習問題1

---
+++ practice1.py
@@ -0,0 +1,10 @@
+# 問題）Camusの文字列を１つずつ出力するには
+
+author = "Camus"
+# 出力なのでprintが使われる
+print(author[0])
+# (この場合文字列のインデックス)[ ] は取り出す時に使われる。
+print(author[1])
+print(author[2])
+print(author[3])
+print(author[4])
